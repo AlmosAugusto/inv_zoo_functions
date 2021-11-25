e28a3afad534a6ea042cb46e115220ff42846fea
almosAugusto <almos.filho@hotmail.com>
2021-11-25 19:06:17 -0300

    Requesito 2 complete

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,5 +3,4 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   return data.species.filter((arrSpecies) => ids.includes(arrSpecies.id));
 }
-
 module.exports = getSpeciesByIds;
