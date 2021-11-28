225bace0a6af3a8f660b715c2efa7232e17d7158
almosAugusto <almos.filho@hotmail.com>
2021-11-28 14:23:21 -0300

    Requesito 9  completed

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((employeeId) => employeeId.id === id).responsibleFor[0];
+  const speciesRes = data.species.find((speciesR) => speciesR.id
+  === employee).residents;
+  const animalAge = speciesRes.reduce((acc, current) => (acc.age < current.age ? current : acc));
+  return Object.values(animalAge);
 }
 
 module.exports = getOldestFromFirstSpecies;
